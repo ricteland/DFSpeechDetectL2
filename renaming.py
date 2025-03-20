8ca876442563d7d3cd8a3bd23ad7007747ff0804
20221120 <j.lleo.terricabras@student.tue.nl>
2025-03-20 01:33:35 +0100

    Function to rename to student or examiner

--- renaming.py
+++ renaming.py
@@ -10,7 +10,7 @@ def rename_audio_files(folder_path, prefix="IELTS"):
     audio_files.sort(key=lambda f: os.path.getctime(os.path.join(folder_path, f)))
 
     # Rename files with format IELTS001, IELTS002, ...
-    for index, file_name in enumerate(audio_files, start=90):
+    for index, file_name in enumerate(audio_files, start=0):
         file_extension = os.path.splitext(file_name)[1]  # Get file extension
         new_name = f"{prefix}{index:03d}{file_extension}"  # Format with leading zeros
         old_path = os.path.join(folder_path, file_name)
@@ -20,5 +20,12 @@ def rename_audio_files(folder_path, prefix="IELTS"):
         print(f"Renamed: {file_name} -> {new_name}")
 
 
+def rename_student_examiner(folder_path, ending = 0):
+    suffix = "_Student" if ending == 0 else "_Examiner"
 
-
+    for filename in os.listdir(folder_path):
+        new_name = f"{filename[:8]}{suffix}.wav"
+        old_path = os.path.join(folder_path, filename)
+        new_path = os.path.join(folder_path, new_name)
+        os.rename(old_path, new_path)
+        print(new_name)
